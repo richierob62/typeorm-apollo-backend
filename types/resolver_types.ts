01e78c8787a74a8bb6d1ac958ac1a36ffb1eb2a8
richierob62 <richierob62@gmail.com>
2021-02-19 23:40:15 -0500

    Remove redis

--- types/resolver_types.ts
+++ types/resolver_types.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from 'express';
 
-import { Redis } from 'ioredis';
-
 export interface Context {
   req: Request;
   res: Response;
-  redis: Redis;
   confirmUrl: string;
 }
 
